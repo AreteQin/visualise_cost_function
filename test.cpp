ef9a5eab164d4cdacf4d3679d06e1d1a7ca35c56
qin <qinqiaomeng@outlook.com>
2024-08-14 18:44:57 -0400

    comparison done

--- test.cpp
+++ test.cpp
@@ -4,9 +4,10 @@
 #include <g2o/core/base_vertex.h>
 #include <g2o/core/base_unary_edge.h>
 #include <g2o/core/block_solver.h>
+#include <g2o/core/optimization_algorithm.h>
 #include <g2o/core/optimization_algorithm_levenberg.h>
 #include <g2o/core/optimization_algorithm_gauss_newton.h>
-//#include <g2o/core/optimization_algorithm_dogleg.h>
+#include <g2o/core/optimization_algorithm_dogleg.h>
 #include <g2o/solvers/dense/linear_solver_dense.h>
 #include <Eigen/Core>
 #include <opencv2/core/core.hpp>
@@ -37,10 +38,12 @@ public:
     // 存盘和读盘：留空
     virtual bool read(istream& in)
     {
+        return true;
     }
 
     virtual bool write(ostream& out) const
     {
+        return true;
     }
 };
 
@@ -73,17 +76,100 @@ public:
 
     virtual bool read(istream& in)
     {
+        return true;
     }
 
     virtual bool write(ostream& out) const
     {
+        return true;
     }
 
 public:
     double _x; // x 值， y 值为 _measurement
 };
 
-int main(int argc, char** argv)
+void DataOptimizer(int OptimizationAlgorithm, double ae, double be, int N, double w_sigma, vector<double> x_data,
+                   vector<double> y_data,
+                   vector<double>& a_estimated, vector<double>& b_estimated, vector<double>& error_estimated)
+{
+    // 构建图优化，先设定g2o
+    typedef g2o::BlockSolver<g2o::BlockSolverTraits<2, 1>> BlockSolverType; // 每个误差项优化变量维度为3，误差值维度为1
+    typedef g2o::LinearSolverDense<BlockSolverType::PoseMatrixType> LinearSolverType; // 线性求解器类型
+
+    // 梯度下降方法，可以从GN, LM, DogLeg 中选
+    auto solver_LM = new g2o::OptimizationAlgorithmLevenberg(
+        std::make_unique<BlockSolverType>(std::make_unique<LinearSolverType>()));
+    auto solver_GN = new g2o::OptimizationAlgorithmGaussNewton(
+        std::make_unique<BlockSolverType>(std::make_unique<LinearSolverType>()));
+    auto solver_DogLeg = new g2o::OptimizationAlgorithmDogleg(
+        std::make_unique<BlockSolverType>(std::make_unique<LinearSolverType>()));
+
+    g2o::SparseOptimizer optimizer; // 图模型
+    optimizer.setVerbose(false); // 调试输出
+
+    if (OptimizationAlgorithm == 0)
+    {
+        optimizer.setAlgorithm(solver_GN);
+    }
+    else if (OptimizationAlgorithm == 1)
+    {
+        optimizer.setAlgorithm(solver_LM);
+    }
+    else if (OptimizationAlgorithm == 2)
+    {
+        optimizer.setAlgorithm(solver_DogLeg);
+    }
+
+    // 往图中增加顶点
+    CurveFittingVertex* v = new CurveFittingVertex();
+    v->setEstimate(Eigen::Vector2d(ae, be));
+    v->setId(0);
+    optimizer.addVertex(v);
+
+    // 往图中增加边
+    for (int i = 0; i < N; i++)
+    {
+        CurveFittingEdge* edge = new CurveFittingEdge(x_data[i]);
+        edge->setId(i);
+        edge->setVertex(0, v); // 设置连接的顶点
+        edge->setMeasurement(y_data[i]); // 观测数值
+        edge->setInformation(Eigen::Matrix<double, 1, 1>::Identity() * 1 / (w_sigma * w_sigma)); // 信息矩阵：协方差矩阵之逆
+        optimizer.addEdge(edge);
+    }
+
+    // 执行优化
+    a_estimated.push_back(ae);
+    b_estimated.push_back(be);
+    error_estimated.push_back(0);
+    for (int i = 0; i < N; i++)
+    {
+        error_estimated[0] += 0.5 * pow((y_data[i] - exp(ae * x_data[i] * x_data[i] + be * x_data[i])), 2);
+    }
+    LOG(INFO) << "start optimization";
+    chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
+    optimizer.initializeOptimization();
+    for (int i = 0; i < 10; i++)
+    {
+        optimizer.optimize(1);
+        // 输出优化值
+        Eigen::Vector2d ab_estimate = v->estimate();
+        LOG(INFO) << "estimated model: " << ab_estimate.transpose();
+        a_estimated.push_back(ab_estimate(0));
+        b_estimated.push_back(ab_estimate(1));
+        double error = 0;
+        for (int i = 0; i < N; i++)
+        {
+            error += 0.5 * pow((y_data[i] - exp(ab_estimate(0) * x_data[i] * x_data[i] + ab_estimate(1) * x_data[i])),
+                               2);
+        }
+        error_estimated.push_back(error);
+    }
+    chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
+    chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
+    LOG(INFO) << "solve time cost = " << time_used.count() << " seconds. ";
+}
+
+int main()
 {
     double ar = 1.0, br = 2.0; // 真实参数值
     double ae = 3.0, be = 3.0; // 估计参数值
@@ -109,59 +195,10 @@ int main(int argc, char** argv)
     matplot::show();
     matplot::hold(matplot::off);
 
-    // 构建图优化，先设定g2o
-    typedef g2o::BlockSolver<g2o::BlockSolverTraits<2, 1>> BlockSolverType; // 每个误差项优化变量维度为3，误差值维度为1
-    typedef g2o::LinearSolverDense<BlockSolverType::PoseMatrixType> LinearSolverType; // 线性求解器类型
-
-    // 梯度下降方法，可以从GN, LM, DogLeg 中选
-    auto solver = new g2o::OptimizationAlgorithmLevenberg(
-        std::make_unique<BlockSolverType>(std::make_unique<LinearSolverType>()));
-    g2o::SparseOptimizer optimizer; // 图模型
-    optimizer.setAlgorithm(solver); // 设置求解器
-    optimizer.setVerbose(false); // 打开调试输出
-
-    // 往图中增加顶点
-    CurveFittingVertex* v = new CurveFittingVertex();
-    v->setEstimate(Eigen::Vector2d(ae, be));
-    v->setId(0);
-    optimizer.addVertex(v);
-
-    // 往图中增加边
-    for (int i = 0; i < N; i++)
-    {
-        CurveFittingEdge* edge = new CurveFittingEdge(x_data[i]);
-        edge->setId(i);
-        edge->setVertex(0, v); // 设置连接的顶点
-        edge->setMeasurement(y_data[i]); // 观测数值
-        edge->setInformation(Eigen::Matrix<double, 1, 1>::Identity() * 1 / (w_sigma * w_sigma)); // 信息矩阵：协方差矩阵之逆
-        optimizer.addEdge(edge);
-    }
-
-    // 执行优化
-    vector<double> a_estimated, b_estimated, error_estimated;
-    a_estimated.push_back(ae);
-    b_estimated.push_back(be);
-    error_estimated.push_back(0);
-    for (int i = 0; i < N; i++)
-    {
-        error_estimated[0] += 0.5*pow((y_data[i]-exp(ae * x_data[i] * x_data[i] + be * x_data[i])),2);
-    }
-    cout << "start optimization" << endl;
-    chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
-    optimizer.initializeOptimization();
-    for (int i = 0; i < 10; i++)
-    {
-        optimizer.optimize(1);
-        // 输出优化值
-        Eigen::Vector2d ab_estimate = v->estimate();
-        LOG(INFO) << "estimated model: " << ab_estimate.transpose();
-        a_estimated.push_back(ab_estimate(0));
-        b_estimated.push_back(ab_estimate(1));
-        error_estimated.push_back(optimizer.activeChi2());
-    }
-    chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
-    chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
-    cout << "solve time cost = " << time_used.count() << " seconds. " << endl;
+    vector<double> a_GN, b_GN, error_GN, a_LM, b_LM, error_LM, a_DogLeg, b_DogLeg, error_DogLeg;
+    DataOptimizer(0, ae, be, N, w_sigma, x_data, y_data, a_GN, b_GN, error_GN);
+    DataOptimizer(1, ae, be, N, w_sigma, x_data, y_data, a_LM, b_LM, error_LM);
+    DataOptimizer(2, ae, be, N, w_sigma, x_data, y_data, a_DogLeg, b_DogLeg, error_DogLeg);
 
     // visualize cost function
     auto [A, B] = matplot::meshgrid(matplot::linspace(ar - 1, ar + 3, N),
@@ -181,11 +218,10 @@ int main(int argc, char** argv)
         }
     }
     auto s = matplot::surf(A, B, Error_sum);
-    // matplot::show();
     matplot::hold(matplot::on);
-    matplot::plot3(a_estimated, b_estimated, error_estimated, "-or");
-
-    // matplot::fplot3(
+    matplot::plot3(a_GN, b_GN, error_GN, "-or");
+    matplot::plot3(a_LM, b_LM, error_LM, "-og");
+    matplot::plot3(a_DogLeg, b_DogLeg, error_DogLeg, "-ob");
     matplot::hold(matplot::off);
     matplot::show();
 
